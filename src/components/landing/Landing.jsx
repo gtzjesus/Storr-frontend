0c904354dbcd75328d3b085e324d4a263f8543a4
gtz.jesus <gtz.jesus@outlook.com>
2024-04-17 10:32:35 -0600

    Working on design + landing component

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -20,10 +20,10 @@ const StyledLanding = styled.div`
   // Code logic for setting the background
   // Design background from Figma software
   background: url('backgrounds/landing.webp');
-  background-size: contain;
+  background-size: cover;
 
   // Code logic for covering full-screen devices
-  min-height: 100vh;
+  min-height: 70vh;
 
   // @MEDIAQUERY logic for bigger devices 980px
   @media (min-width: 61.25em) {
