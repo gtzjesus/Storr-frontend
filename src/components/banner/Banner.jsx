6b4d06067a1a31556a65d9da3cdb500e31ef61b0
gtz.jesus <gtz.jesus@outlook.com>
2024-04-18 12:38:31 -0600

    Ad implementation for new section + bug fixes

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -10,10 +10,10 @@ const scroll = keyframes`
   }
 `;
 
-// Styled component for the logo container
-const LogoContainer = styled.div`
+// Styled component for the Sponsor container
+const SponsorContainer = styled.div`
   display: flex;
-  width: 200%; // Double the width to accommodate duplicate logos
+  width: 200%; // Double the width to accommodate duplicate Sponsors
   overflow: hidden; // Hide overflow to prevent horizontal scrollbar
   white-space: nowrap;
   padding: var(--padding-medium) 0;
@@ -22,42 +22,29 @@ const LogoContainer = styled.div`
   animation: ${scroll} 5s linear infinite;
 `;
 
-const Logo = styled.img`
-  height: 100%;
+const Sponsor = styled.img`
+  height: 2.5rem;
+  padding-right: var(--padding-medium);
 `;
 
 // Component
 function Banner() {
   return (
-    <LogoContainer>
-      {/* Add the list of logos and repeat the list for a seamless loop */}
-      <Logo src="/sponsors/kongster.webp" alt="Kongster Logo" />
-      <Logo src="/sponsors/dxl.webp" alt="DXL Logo" />
-      <Logo src="/sponsors/union.webp" alt="Union Logo" />
-      <Logo src="/sponsors/polanco.webp" alt="Polanco Logo" />
-      <Logo src="/sponsors/pallares.webp" alt="Pallares Logo" />
-      <Logo src="/sponsors/shaellers.webp" alt="Shaellers Logo" />
-      <Logo src="/sponsors/baja.webp" alt="Baja Logo" />
-      <Logo src="/sponsors/rezas.webp" alt="Rezas Logo" />
-      <Logo src="/sponsors/utv.webp" alt="Utv Logo" />
-      <Logo src="/sponsors/smh.webp" alt="SMH Logo" />
-      <Logo src="/sponsors/proauto.webp" alt="Proauto Logo" />
-      <Logo src="/sponsors/fina.webp" alt="Fina Logo" />
-
-      {/* Duplicate the list for seamless loop */}
-      <Logo src="/sponsors/kongster.webp" alt="Kongster Logo" />
-      <Logo src="/sponsors/dxl.webp" alt="DXL Logo" />
-      <Logo src="/sponsors/union.webp" alt="Union Logo" />
-      <Logo src="/sponsors/polanco.webp" alt="Polanco Logo" />
-      <Logo src="/sponsors/pallares.webp" alt="Pallares Logo" />
-      <Logo src="/sponsors/shaellers.webp" alt="Shaellers Logo" />
-      <Logo src="/sponsors/baja.webp" alt="Baja Logo" />
-      <Logo src="/sponsors/rezas.webp" alt="Rezas Logo" />
-      <Logo src="/sponsors/utv.webp" alt="Utv Logo" />
-      <Logo src="/sponsors/smh.webp" alt="SMH Logo" />
-      <Logo src="/sponsors/proauto.webp" alt="Proauto Logo" />
-      <Logo src="/sponsors/fina.webp" alt="Fina Logo" />
-    </LogoContainer>
+    <SponsorContainer>
+      {/* Add the list of Sponsors and repeat the list for a seamless loop */}
+      <Sponsor src="/sponsors/kongster.webp" alt="Kongster Sponsor" />
+      <Sponsor src="/sponsors/dxl.webp" alt="DXL Sponsor" />
+      <Sponsor src="/sponsors/union.webp" alt="Union Sponsor" />
+      <Sponsor src="/sponsors/polanco.webp" alt="Polanco Sponsor" />
+      <Sponsor src="/sponsors/pallares.webp" alt="Pallares Sponsor" />
+      <Sponsor src="/sponsors/shaellers.webp" alt="Shaellers Sponsor" />
+      <Sponsor src="/sponsors/baja.webp" alt="Baja Sponsor" />
+      <Sponsor src="/sponsors/rezas.webp" alt="Rezas Sponsor" />
+      <Sponsor src="/sponsors/utv.webp" alt="Utv Sponsor" />
+      <Sponsor src="/sponsors/smh.webp" alt="SMH Sponsor" />
+      <Sponsor src="/sponsors/proauto.webp" alt="Proauto Sponsor" />
+      <Sponsor src="/sponsors/fina.webp" alt="Fina Sponsor" />
+    </SponsorContainer>
   );
 }
 
