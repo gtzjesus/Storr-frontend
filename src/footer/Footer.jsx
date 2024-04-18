65ab42b08ee16210491db8ae9016cddc9d8f936e
gtz.jesus <gtz.jesus@outlook.com>
2024-04-18 13:19:36 -0600

    Footer Implementation

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -81,9 +81,33 @@ function Footer() {
       </Signature>
 
       <Signature>
-        <Sponsor src="/sponsors/kongster.webp" alt="Kongster Sponsor"></Sponsor>
-        <Sponsor src="/sponsors/dxl.webp" alt="DXL Sponsor"></Sponsor>
-        <Sponsor src="/sponsors/union.webp" alt="Union Sponsor"></Sponsor>
+        <Sponsor src="/sponsors/kongster.webp" alt="Kongster Sponsor" />
+        <Sponsor src="/sponsors/dxl.webp" alt="DXL Sponsor" />
+      </Signature>
+
+      <Signature>
+        <Sponsor src="/sponsors/union.webp" alt="Union Sponsor" />
+      </Signature>
+
+      <Signature>
+        <Sponsor src="/sponsors/polanco.webp" alt="Polanco Sponsor" />
+        <Sponsor src="/sponsors/shaellers.webp" alt="Shaellers Sponsor" />
+      </Signature>
+
+      <Signature>
+        <Sponsor src="/sponsors/baja.webp" alt="Baja Sponsor" />
+        <Sponsor src="/sponsors/pallares.webp" alt="Pallares Sponsor" />
+      </Signature>
+
+      <Signature>
+        <Sponsor src="/sponsors/rezas.webp" alt="Rezas Sponsor" />
+        <Sponsor src="/sponsors/utv.webp" alt="Utv Sponsor" />
+        <Sponsor src="/sponsors/smh.webp" alt="SMH Sponsor" />
+      </Signature>
+
+      <Signature>
+        <Sponsor src="/sponsors/proauto.webp" alt="Proauto Sponsor" />
+        <Sponsor src="/sponsors/fina.webp" alt="Fina Sponsor" />
       </Signature>
 
       <Signature>
