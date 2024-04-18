42d0ee0b384a5dc0a1f82e2fa857108fe2756682
gtz.jesus <gtz.jesus@outlook.com>
2024-04-18 12:12:57 -0600

    Ad implementation for new section + bug fixes

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,51 +1,50 @@
-// ------------------------------
-// File: Banner.js
-// ------------------------------
-// Description: React component for displaying moving horizontal banner with multiple texts HOUSES.
-// ------------------------------
-
-// ------------------------------
-// Imports
-// ------------------------------
-// This section has all necessary imports for this component.
 import styled, { keyframes } from 'styled-components';
 
-// ------------------------------
-// Styled Components
-// ------------------------------
-// This section has all CSS styles configured for every HTML element.
-const moveText = keyframes`
+// Define the animation for continuous scrolling
+const scroll = keyframes`
   0% {
-    transform: translateX(-100%);
+    transform: translateX(0%);
   }
   100% {
-    transform: translateX(100%);
+    transform: translateX(-100%);
   }
 `;
 
 // Styled component for the logo container
 const LogoContainer = styled.div`
   display: flex;
-  width: fit-content;
-  margin: 0 auto;
+  width: 200%; // Double the width to accommodate duplicate logos
   overflow: hidden; // Hide overflow to prevent horizontal scrollbar
   white-space: nowrap;
   padding: var(--padding-medium) 0;
-  animation: ${moveText} 15s linear infinite alternate; // Apply the animation
+
+  // Apply the continuous scroll animation
+  animation: ${scroll} linear infinite;
 `;
 
 const Logo = styled.img`
-  padding-right: 35px;
-  height: 8.5vh;
+  height: 100%;
 `;
 
-// ------------------------------
 // Component
-// ------------------------------
-// This section has our React Component which handles the hook data
 function Banner() {
   return (
     <LogoContainer>
+      {/* Add the list of logos and repeat the list for a seamless loop */}
+      <Logo src="/sponsors/kongster.webp" alt="Kongster Logo" />
+      <Logo src="/sponsors/dxl.webp" alt="DXL Logo" />
+      <Logo src="/sponsors/union.webp" alt="Union Logo" />
+      <Logo src="/sponsors/polanco.webp" alt="Polanco Logo" />
+      <Logo src="/sponsors/pallares.webp" alt="Pallares Logo" />
+      <Logo src="/sponsors/shaellers.webp" alt="Shaellers Logo" />
+      <Logo src="/sponsors/baja.webp" alt="Baja Logo" />
+      <Logo src="/sponsors/rezas.webp" alt="Rezas Logo" />
+      <Logo src="/sponsors/utv.webp" alt="Utv Logo" />
+      <Logo src="/sponsors/smh.webp" alt="SMH Logo" />
+      <Logo src="/sponsors/proauto.webp" alt="Proauto Logo" />
+      <Logo src="/sponsors/fina.webp" alt="Fina Logo" />
+
+      {/* Duplicate the list for seamless loop */}
       <Logo src="/sponsors/kongster.webp" alt="Kongster Logo" />
       <Logo src="/sponsors/dxl.webp" alt="DXL Logo" />
       <Logo src="/sponsors/union.webp" alt="Union Logo" />
